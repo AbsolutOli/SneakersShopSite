4aff1b5ca4c2840649e4cc18fa6b5132f242e4a7
AbsolutOli <vocameoli@gmail.com>
2023-08-06 19:33:12 +0300

    Added CSS to header block

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return <div className="wrapper">
     <header>
       <div className="headerLeft">
-        <svg />
+        <img width={40} height={40} src="/img/logo.png" alt="Logo" />
         <div className="headerInfo">
           <h3>Sneakers Shop</h3>
           <p>Магазин лучших кроссовок</p>
@@ -10,17 +10,18 @@ function App() {
       </div>
       <ul className="headerRight">
         <li>
-          <svg />
+          <img width={18} height={17} src="/img/cart.svg" alt="Cart Logo" />
           <span>1205 руб.</span>
         </li>
         <li>
-          <svg />
-        </li>
-        <li>
-          <svg />
+          <img width={18} height={17} src="/img/user.svg" alt="User Logo" />
         </li>
       </ul>
     </header>
+    <div className="content">
+      <h1>Все кроссовки</h1>
+      ...
+    </div>
   </div>;
 }
 
