d8ebb2d6a1e040a4f583672d98b2c3b91007c4c3
AbsolutOli <vocameoli@gmail.com>
2023-08-07 17:22:25 +0300

    Added HTML-structure to side-panel block

--- src/App.js
+++ src/App.js
@@ -22,6 +22,47 @@ function App() {
       </ul>
     </header>
     <div className="content">
+      <div className="side-panel">
+        <div className="panel">
+          <h3>Корзина</h3>
+          <div className="cart-items">
+            <div className="cart-item">
+              <div className="cart-items__image">
+                <img src="/img/sneakers/2.jpg" alt="Sneakers 1" />
+              </div>
+              <div className="cart-items__info">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>5199.60 грн.</b>
+              </div>
+              <button>
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB" />
+                  <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="cart-prices">
+            <div className="price">
+              <p>Итого: </p>
+              <span></span>
+              <b>5199.60 грн.</b>
+            </div>
+            <div className="price">
+              <p>Налог 5%: </p>
+              <span></span>
+              <b>42.90 грн.</b>
+            </div>
+          </div>
+          <button>
+            <div className="btnContent">
+              <p>Оформить заказ</p>
+              <img src="/img/arrowRight.svg" alt="Arrow Icon" />
+            </div>
+          </button>
+        </div>
+      </div>
+
       <div className="search-sneakers-block">
         <h1>Все кроссовки</h1>
         <div>
