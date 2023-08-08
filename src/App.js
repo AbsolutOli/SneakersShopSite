3ec3a5ffa71dff33afe40f848528012c7ce826c8
AbsolutOli <vocameoli@gmail.com>
2023-08-08 17:56:20 +0300

    Created and imported component Header

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import Card from './components/Card';
 import SidePanel from './components/SidePanel';
+import Header from './components/Header';
 
 
 function App() {
   return <div className="wrapper">
     <SidePanel />
 
-    <header>
-      <div className="headerLeft">
-        <img width={40} height={40} src="/img/logo.png" alt="Logo" />
-        <div className="headerInfo">
-          <h3>Sneakers Shop</h3>
-          <p>Магазин лучших кроссовок</p>
-        </div>
-      </div>
-      <ul className="headerRight">
-        <li>
-          <img width={18} height={17} src="/img/cart.svg" alt="Cart Logo" />
-          <span>1205 руб.</span>
-        </li>
-        <li>
-          <img width={21} height={19} src="/img/heart.svg" alt="Heart Logo" />
-        </li>
-        <li>
-          <img width={20} height={20} src="/img/user.svg" alt="User Logo" />
-        </li>
-      </ul>
-    </header>
+    <Header />
+
     <div className="content">
 
       <div className="search-sneakers-block">
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+function Header() {
+    return <header>
+        <div className="headerLeft">
+            <img width={40} height={40} src="/img/logo.png" alt="Logo" />
+            <div className="headerInfo">
+                <h3>Sneakers Shop</h3>
+                <p>Магазин лучших кроссовок</p>
+            </div>
+        </div>
+        <ul className="headerRight">
+            <li>
+                <img width={18} height={17} src="/img/cart.svg" alt="Cart Logo" />
+                <span>1205 руб.</span>
+            </li>
+            <li>
+                <img width={21} height={19} src="/img/heart.svg" alt="Heart Logo" />
+            </li>
+            <li>
+                <img width={20} height={20} src="/img/user.svg" alt="User Logo" />
+            </li>
+        </ul>
+    </header>
+}
+
+export default Header;
