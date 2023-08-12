f82b0e6bf797de96bee04e41af1396e9a7c7c28b
AbsolutOli <vocameoli@gmail.com>
2023-08-12 14:33:21 +0300

    Added searching sneakers by their title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header';
 function App() {
   const [cardsArr, setCardsArr] = React.useState([]);
   const [cartCardsArr, setCartCardsArr] = React.useState([]);
+  const [searchValue, setSearcValue] = React.useState('');
+
   React.useEffect(() => {
     fetch('https://64d608f7754d3e0f13617faa.mockapi.io/items')
       .then((res) => res.json())
@@ -18,6 +20,10 @@ function App() {
     setCartCardsArr((prev) => [...prev, obj]);
   }
 
+  const onChangeInputValue = (event) => {
+    setSearcValue(event.target.value);
+  }
+
   return <div className="wrapper">
     {cartState && <SidePanel items={cartCardsArr} onClose={() => setCartState(false)} />}
 
@@ -26,16 +32,16 @@ function App() {
     <div className="content">
 
       <div className="search-sneakers-block">
-        <h1>Все кроссовки</h1>
+        <h1>{searchValue ? `Результат поиска "${searchValue}"` : "Все кроссовки"}</h1>
         <div>
-          <img src="/img/search.svg" alt="Search Icon" />
-          <input placeholder="Поиск..." />
+          {searchValue ? <button onClick={() => (setSearcValue(''))}><img width={16} height={16} src="/img/cross.svg" alt="Close" /></button> : <img src="/img/search.svg" alt="Search Icon" />}
+          <input onChange={onChangeInputValue} placeholder="Поиск..." value={searchValue} />
         </div>
       </div>
 
       <div className="sneakers">
-        {cardsArr.map((card) => (
-          <Card image={card.image} title={card.title} price={card.price} onPlus={(obj) => onAddToCart(obj)} />
+        {cardsArr.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase())).map((card, index) => (
+          <Card key={index} image={card.image} title={card.title} price={card.price} onPlus={(obj) => onAddToCart(obj)} />
         ))}
       </div>
 
