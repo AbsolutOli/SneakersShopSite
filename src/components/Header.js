ca7f0a7a41eeb426cf3510cee3c73de0e5a3cd7c
AbsolutOli <vocameoli@gmail.com>
2023-08-21 17:31:44 +0300

    Change directions in Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,12 @@ function Header(props) {
                 <img width={18} height={17} src="img/cart.svg" alt="Cart Logo" />
                 <span>{Number(totalPrice).toFixed(2)} грн.</span>
             </li>
-            <Link to="/favorites/">
+            <Link to="favorites/">
                 <li>
                     <img width={21} height={19} src="img/heart.svg" alt="Heart Logo" />
                 </li>
             </Link>
-            <Link to="/orders/">
+            <Link to="orders/">
                 <li>
                     <img width={20} height={20} src="img/user.svg" alt="User Logo" />
                 </li>
